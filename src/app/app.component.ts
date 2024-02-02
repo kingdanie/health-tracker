63084324d56a94984491938c500971e242a5893d
kingdanie <daniedemola@gmail.com>
2024-02-02 19:41:46 +0100

    added kanban and modal components to the app.ts file

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,13 +1,27 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { KanbanBoardComponent } from "./components/kanban-board/kanban-board.component";
+import { DeleteModalComponent } from './components/delete-modal/delete-modal.component';
+import { CreateModalComponent } from './components/create-modal/create-modal.component';
+import { ModalService } from './services/modal.service';
+import { CommonModule } from '@angular/common';
+import { TaskModalComponent } from './components/task-modal/task-modal.component';
 
 @Component({
-  selector: 'app-root',
-  standalone: true,
-  imports: [RouterOutlet],
-  templateUrl: './app.component.html',
-  styleUrl: './app.component.css'
+    selector: 'app-root',
+    standalone: true,
+    templateUrl: './app.component.html',
+    styleUrl: './app.component.css',
+    imports: [
+      RouterOutlet, CommonModule, KanbanBoardComponent, DeleteModalComponent, 
+      CreateModalComponent, TaskModalComponent
+    ]
 })
 export class AppComponent {
   title = 'health-tracker';
+
+  constructor(
+    public modalService: ModalService
+  ) {}
+
 }
