f4c68d19d74f155b6919021695e85d39139e056e
kingdanie <daniedemola@gmail.com>
2024-02-02 23:37:47 +0100

    ui improvements to app header and task modal

--- src/app/components/task-modal/task-modal.component.ts
+++ src/app/components/task-modal/task-modal.component.ts
@@ -2,11 +2,13 @@ import { Component, Input } from '@angular/core';
 import { ModalService } from '../../services/modal.service';
 import { Task } from '../../interfaces/task.model';
 import { TaskService } from '../../services/task.service';
+import { FontAwesomeModule } from '@fortawesome/angular-fontawesome';
+import { faCalendarDay, faTimes, faTrash } from '@fortawesome/free-solid-svg-icons';
 
 @Component({
   selector: 'app-task-modal',
   standalone: true,
-  imports: [],
+  imports: [FontAwesomeModule],
   templateUrl: './task-modal.component.html',
   styleUrl: './task-modal.component.css'
 })
@@ -15,13 +17,19 @@ export class TaskModalComponent {
   TaskItem! : Task;
 
   constructor(public modalService: ModalService, private taskService : TaskService) {}
-
+  faTrash = faTrash;
+  faTimes = faTimes;
+  faCalendarDay = faCalendarDay;
   closeModal(){
     this.modalService.closeModal();
   }
 
   deleteModal() {
     this.modalService.openDeleteTaskModal();
+  }
+
+  editTask() {
+    this.modalService.openEditTaskModal();
   }
 
 ngOnInit() {
